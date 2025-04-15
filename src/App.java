d29c80a42b771f75187a5e15a99a4984977dfa1a
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-04-14 21:17:36 -0300

    feat: implement SuperCalculator UI and update main application entry

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import ui.SuperCalculator;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        new SuperCalculator();
     }
 }
--- src/ui/SuperCalculator.java
+++ src/ui/SuperCalculator.java
@@ -1,5 +1,13 @@
 package ui;
 
-public class SuperCalculator {
-    
+import javax.swing.JFrame;
+
+public class SuperCalculator extends JFrame {
+    public SuperCalculator() {
+        setTitle("Super Calculator");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
 }
